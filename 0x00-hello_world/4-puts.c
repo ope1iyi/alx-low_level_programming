890ae54d54ffba68ff868904cda62bb31412266b
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-13 16:26:32 -0700

    adding task 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
- putc("\"Programming is like building a multilingual puzzle\n",stdout);
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
