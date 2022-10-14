4f58e86dd212bc90f2aa5cb773c6cc12b5f9d14d
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-13 17:15:43 -0700

    .c file corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main(void)
-{
 /**
  * main - Entry point
  *
- * Return: Always 0  (succes)
+ * Return: Always 0 (success)
  */
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
