cc8705f26128e986eff6652d4346e578b013647b
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-13 17:50:03 -0700

    adding sizw

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int;
+float;
+long int;
+long long int;
+char;
+printf("Size of char: %ld", sizeof(char));
+printf("Size of int: %ld", sizeof(int));
+printf("Size of long int: %ld", sizeof(long int));
+printf("Size of long long int: %ld", sizeof(long long int));
+printf("Size of float: %ld", sizeof(float);
+return(0);
+}
