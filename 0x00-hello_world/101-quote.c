40bf5418c4b569fb6a56139a7f5dd48a09076a44
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-14 04:36:39 -0700

    task 6 and 101 ammended:

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /*
  * Main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 on (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return(1);
 }
