543a21bba6cc5e693ff80fa495f4d9a49bf1392b
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-14 13:55:16 -0700

    betty 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * Main - Entry point
- *
+/**
+ * Main - prints exactly  "and that piece of art is useful\"
+ * - Dora Korpar, 2015-10-19\n"
+ * to the stdard output
  * Return: Always 1 on (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
