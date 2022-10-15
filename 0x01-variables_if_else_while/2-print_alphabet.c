e45521802baffed66ec43e00bb74e5b9fa4e95e4
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-14 18:00:45 -0700

    printed alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - prints the alphabets in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char beg = 'a';
+while (beg <= 'z')
+{
+putchar(beg);
+   beg++;
+}
+putchar('\n');
+return (0);
+}
