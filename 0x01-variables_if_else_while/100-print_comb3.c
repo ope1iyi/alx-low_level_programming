0864beb5f873784887e744dee1009bc08fcb70e0
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-17 01:09:17 -0700

    100 done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 int num1, num2;
 for (num1 = 48; num1 < 57; num1++)
 {
-for (num2 = 49; num2 <= 57; num2++)
+for (num2 = num1 + 1; num2 <= 57; num2++)
 {
 putchar(num1);
 putchar(num2);
