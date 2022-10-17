c4b38728b65661821c9b4d909f08f4b2e3920fd8
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-17 03:00:58 -0700

    101 and 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,17 @@
 int main(void)
 {
 int num1, num2;
-for (num1 = 48; num1 <= 57; num1++)
+for (num1 = 0; num1 <= 98; num1++)
 {
-for (num2 = 49; num2 <= 57; num2++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-putchar(num1);
-putchar(num2);
-if (num1 == 57 && num2 == 57)
+putchar((num1 / 10) + '0');
+putchar((num2 % 10) + '0');
+putchar(' ');
+putchar((num1 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
 continue;
 putchar(44);
 putchar(32);
