53caa70cd5d66574c03947b3d7bff8566b5360cf
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-18 08:42:46 -0700

    task 4 aplha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet
+ *
+ *
+ * Return: 0
+ */
+int _isalpha(int c);
+{
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		char u;
+		for (u = 'A'; u <= 'Z'; u++)
+		{
+			if (c == l || c == u)
+				return (1);
+		}
+	}
+}
