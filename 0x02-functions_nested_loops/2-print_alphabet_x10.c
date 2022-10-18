10a37d7325236e6d33fa6eb3a0d315e0c6b1e909
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-18 07:42:04 -0700

    commit task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabets
+ * in lowercase 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	i = 0;
+
+	while (i < 10)
+	{
+		char start;
+
+		for (start = 'a'; start <= 'z'; start++)
+			_putchar(start);
+	}
+	_putchar('\n');
+}
