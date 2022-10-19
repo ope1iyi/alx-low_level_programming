a572bec717c7a934d851ff19dcc74329cd25745d
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-19 02:34:39 -0700

    commit 7 and 9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two intergers and return the result
+ * @n1: receives interger
+ * @n2: receives interger
+ * Return: 0
+ */
+int add(int n1, int n2)
+{
+	int result;
+
+	result = n1 + n2;
+	return (result);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,7 @@
 int print_last_digit(int num)
 {
 	int last = num % 10;
+
 	if (num < 0)
 	{
 		last = last * -1;
