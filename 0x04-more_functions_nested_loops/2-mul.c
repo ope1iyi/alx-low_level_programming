f25615e9a653d1e7ab19b881b149ad3c6b893d36
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-21 05:15:40 -0700

    multiply task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two interger
+ * @a: receives interger
+ * @b: receives interger
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
