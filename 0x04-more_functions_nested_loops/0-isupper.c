fa01902db79ca27a60c250df7cffe1394ab0da1b
ope1iyi <opeyemiiyiola534@gmail.com>
2022-10-21 03:58:24 -0700

    adding task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case character
+ * @c: receives interger input
+ * Return: 1
+ */
+int _isupper(int c)
+{
+	char upper = 'A';
+
+	while (upper <= 'Z')
+	{
+		if (c == upper)
+			return (1);
+		else
+			return (0);
+		upper++;
+	}
+	return (0);
+}
