e6d1be2e4a63de3f859e62a00ddd361e3378361f
ope1iyi <opeyemiiyiola534@gmail.com>
2022-11-08 23:33:39 -0800

    adding string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - give the length of a string
+ * @s: receives the string
+ *
+ * Return: the length of th string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		return (i);
+}
