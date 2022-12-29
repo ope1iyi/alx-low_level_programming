e573a6fd8c997a65958f19433db639445657fbb6
ope1iyi <opeyemiiyiola534@gmail.com>
2022-12-29 05:34:34 -0800

    concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenate strings
+ * @dest: destination string
+ * @src: source string
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + J] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
